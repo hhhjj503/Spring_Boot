ee32e9294cf3546e0176ceb28acb6a182e4e684c
hhj503 <kil312055@gmail.com>
2021-02-08 01:01:56 +0900

    20210208

---
+++ SpringWeb/src/main/java/com/newlec/web/controller/admin/NoticeController.java
@@ -0,0 +1,27 @@
+package com.newlec.web.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("adminNoticeController")
+@RequestMapping("/admin/board/notice/")
+public class NoticeController {
+	
+	@GetMapping("list")
+	public String list(Model model) {
+		model.addAttribute("test","hello devTools");
+		return "admin/board/notice/list";
+	}
+	  
+	@GetMapping("detail")
+	public String detail() {
+		return "admin/board/notice/detail";
+	}
+	
+	@GetMapping("reg")
+	public String reg() {
+		return "admin/board/notice/reg";
+	}
+}
---
+++ SpringWeb/src/main/java/com/newlec/web/controller/customer/NoticeController.java
@@ -0,0 +1,23 @@
+package com.newlec.web.controller.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer/notice/")
+public class NoticeController {
+
+	@GetMapping("list")
+	public String list(Model model) {
+		model.addAttribute("test","hello devTools");
+		return "customer/notice/list";
+	}
+	  
+	@GetMapping("detail")
+	public String detail() {
+		return "customer/notice/detail";
+	}
+	
+}
